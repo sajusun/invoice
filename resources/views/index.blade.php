5f04034a7b35978e1feecaa54bcde27ccc551d99
sajusun <sajuislam266@gmail.com>
2025-02-03 02:42:33 +0600

    add html/css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>'Invozen' - Simple Invoice Generator</title>
-    <link rel="icon" type="image/x-icon" href="invozen-fabicon.png">
+    @include('custom-layouts.headTagContent')
+
+    <title>invozen is your invoice maker</title>
+
     <style>
         /* Reset some default styling */
         * {
