974b8733f04472905a5f3bbdaa3caf036f643659
sajusun <sajuislam266@gmail.com>
2025-11-28 16:18:43 +0600

    beefore commit

---
+++ config/dompdf.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    'show_warnings' => true,   // Throw an Exception on warnings from dompdf
+    'public_path' => null,  // Override the public path if needed
+    'convert_entities' => true,
+    'options' => [
+        'font_dir' => storage_path('fonts'),
+        'font_cache' => storage_path('fonts'),
+        'temp_dir' => sys_get_temp_dir(),
+        'chroot' => realpath(base_path()),
+        'allowed_protocols' => [
+            'data://' => ['rules' => []],
+            'file://' => ['rules' => []],
+            'http://' => ['rules' => []],
+            'https://' => ['rules' => []],
+        ],
+        'artifactPathValidation' => null,
+        'log_output_file' => null,
+        'enable_font_subsetting' => false,
+        'pdf_backend' => 'CPDF',
+        'default_media_type' => 'screen',
+        'default_paper_size' => 'a4',
+        'default_paper_orientation' => 'portrait',
+        'default_font' => 'serif',
+        'dpi' => 96,
+        'enable_php' => false,
+        'enable_javascript' => true,
+        'enable_remote' => true,
+        'allowed_remote_hosts' => null,
+        'font_height_ratio' => 1.1,
+        'enable_html5_parser' => true,
+    ],
+
+];
